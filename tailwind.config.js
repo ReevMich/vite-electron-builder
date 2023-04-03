db080b8b55f0c9672f1546cf875d0e0eb49d2dd6
Michael Reeves <Mikeblacknes@gmail.com>
2023-04-02 22:19:09 -0500

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+const shardTWConfig = require('./shared/tailwind');
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['packages/renderer/src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: shardTWConfig.theme,
+  plugins: [],
+};
+
